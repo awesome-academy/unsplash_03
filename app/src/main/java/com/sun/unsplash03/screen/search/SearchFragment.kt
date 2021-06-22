d65e5ad77a4eae50b3e094007f74af1fbbc13cb4
Luong Tran <tran.huu.luong@sun-asterisk.com>
2021-06-22 09:09:55 +0700

    create ui search

--- app/src/main/java/com/sun/unsplash03/screen/search/SearchFragment.kt
+++ app/src/main/java/com/sun/unsplash03/screen/search/SearchFragment.kt
@@ -1,8 +1,12 @@
 package com.sun.unsplash03.screen.search
 
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.widget.EditText
+import androidx.appcompat.widget.SearchView
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.sun.unsplash03.R
 import com.sun.unsplash03.data.model.Collection
 import com.sun.unsplash03.data.model.Photo
@@ -11,9 +15,8 @@ import com.sun.unsplash03.screen.collection.adapter.CollectionAdapter
 import com.sun.unsplash03.screen.collection_photo.CollectionPhotoFragment
 import com.sun.unsplash03.screen.detail.DetailFragment
 import com.sun.unsplash03.screen.photo.adapter.PhotoAdapter
-import com.sun.unsplash03.utils.BottomType
 import com.sun.unsplash03.utils.base.BaseFragment
-import com.sun.unsplash03.utils.ext.replaceFragment
+import com.sun.unsplash03.utils.ext.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
@@ -22,7 +25,6 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
     private val collectionAdapter by lazy { CollectionAdapter(::clickItemCollection) }
 
     override val viewModel: SearchViewModel by viewModel()
-
     override fun inflateViewBinding(inflater: LayoutInflater) =
         FragmentSearchBinding.inflate(inflater)
 
@@ -39,9 +41,37 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
         arguments?.run {
             viewModel.setTypeSearch(getInt(ARGUMENT_TYPE_SEARCH))
         }
+        viewBinding.run {
+            (recyclerPhotoSearch.layoutManager as StaggeredGridLayoutManager).gapStrategy =
+                StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+            searchView.run {
+                requestFocus()
+                setOnQueryTextFocusChangeListener { _, hasFocus ->
+                    if (hasFocus) {
+                        showKeyboard(requireActivity())
+                    }
+                }
+                setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        query?.let {
+                            this@SearchFragment.viewModel.searchContent(query)
+                            viewBinding.searchView.clearFocus()
+                        }
+                        return false
+                    }
+
+                    override fun onQueryTextChange(newText: String?) = false
+                })
+                toolbarSearch.setNavigationOnClickListener {
+                    parentFragmentManager.popBackStack()
+                    hideKeyboard(requireActivity())
+                }
+            }
+        }
     }
 
     override fun registerLiveData() = with(viewModel) {
+        super.registerLiveData()
         photos.observe(viewLifecycleOwner, Observer(::updatePhotos))
         collections.observe(viewLifecycleOwner, Observer(::updateCollections))
     }
@@ -51,10 +81,18 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
     }
 
     private fun updatePhotos(photos: MutableList<Photo>) {
+        viewBinding.run {
+            recyclerCollectionSearch.toGone()
+            recyclerPhotoSearch.toVisible()
+        }
         photoAdapter.updateData(photos)
     }
 
     private fun updateCollections(collections: MutableList<Collection>) {
+        viewBinding.run {
+            recyclerCollectionSearch.toVisible()
+            recyclerPhotoSearch.toGone()
+        }
         collectionAdapter.submitList(collections)
     }
 
@@ -66,9 +104,7 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>() {
         private const val ARGUMENT_TYPE_SEARCH = "ARGUMENT_TYPE_SEARCH"
 
         fun newInstance(typeSearch: Int) = SearchFragment().apply {
-            arguments = bundleOf(
-                ARGUMENT_TYPE_SEARCH to typeSearch
-            )
+            arguments = bundleOf(ARGUMENT_TYPE_SEARCH to typeSearch)
         }
     }
 }
