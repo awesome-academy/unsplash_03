86b978eced509656a0608a54752d2d242a14bd78
Luong Tran <tran.huu.luong@sun-asterisk.com>
2021-06-22 09:09:55 +0700

    add splash screen

--- app/src/main/java/com/sun/unsplash03/MainActivity.kt
+++ app/src/main/java/com/sun/unsplash03/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.sun.unsplash03
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.ThemeUnsplash03)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
